22fb95d08aa86df506568de2b1286f3a71a116a5
uvsq22103431 <92145272+iDillan@users.noreply.github.com>
2022-04-13 15:50:13 +0200

    Add files via upload
    premiere version avec des lignes

--- code.py
+++ code.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 import random as rd
-###Constantes
-LARGEUR = 1600
-HAUTEUR = 900
 
+
+#23lignes
+LARGEUR = 1200
+HAUTEUR = 600
 racine = tk.Tk()
 canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
+
+ligne = canvas.create()
+for i in range (30, 1200, 52):
+    line = canvas.create_line((int(i),10),(int(i),600),fill='red',width=5)
+
+
+
 canvas.grid()
 canvas.pack()
 
