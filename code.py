0e546daf3330ab2dd6134e976e13c4e27cc09b8b
Maxime MANGNAN <maxime.mangnan@ens.uvsq.fr>
2022-04-13 16:10:15 +0200

    Ajout lignes extérieures

--- code.py
+++ code.py
@@ -1,11 +1,22 @@
 import tkinter as tk
 import random as rd
 
-LARGEUR = 1600
-HAUTEUR = 900
+
+#23lignes
+LARGEUR = 1200
+HAUTEUR = 600
 racine = tk.Tk()
 canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
+
+for i in range (30, 1200, 52):
+    line = canvas.create_line((int(i),10),(int(i),600),fill='red',width=5)
+
+line2 = canvas.create_line(0,0,0,HAUTEUR,fill='red',width=20)
+line3 = canvas.create_line(0,0,LARGEUR,0,fill='red',width=20)
+line4 = canvas.create_line(0,HAUTEUR,LARGEUR,HAUTEUR,fill='red',width=10)
+line5 = canvas.create_line(LARGEUR,0,LARGEUR,HAUTEUR,fill='red',width=10)
+
 canvas.grid()
 canvas.pack()
 
-racine.mainloop()
+tk.mainloop()
