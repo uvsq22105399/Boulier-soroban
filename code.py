03f26c67fb3ff7610fe3bfa15f3dbe9afe7e70ae
Maxime MANGNAN <maxime.mangnan@ens.uvsq.fr>
2022-04-13 15:26:12 +0200

    2e commit

--- code.py
+++ code.py
@@ -1,3 +1,11 @@
 import tkinter as tk
 import random as rd
 
+LARGEUR = 600
+HAUTEUR = 600
+racine = tk.Tk()
+canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
+canvas.grid()
+canvas.pack()
+
+racine.mainloop()
