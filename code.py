bec42417ef6f28ba7ac8295728682a1f44e37b1f
Sophie FABRET <sophie.fabret@uvsq.fr>
2022-04-13 15:34:48 +0200

    Test

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 import random as rd
-
+###Constantes
 LARGEUR = 1600
 HAUTEUR = 900
+
 racine = tk.Tk()
 canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
 canvas.grid()
